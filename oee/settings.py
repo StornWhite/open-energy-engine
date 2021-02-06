740b4479f1efd7c96df5196e8490b153e22b2b78
Storn White <stornwhite@Storns-Awesome-Computer-Box.local>
2021-02-06 06:08:44 -0800

    [OEE-6] PostgreSQL database setup

--- oee/settings.py
+++ oee/settings.py
@@ -85,7 +85,18 @@ WSGI_APPLICATION = 'oee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get(
+            "SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")
+        ),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
 
 
 # Password validation
